2b700e59207df7cfa49c1f076fa4289b931abf6f
Zack Koppert <zkoppert@github.com>
2021-01-06 14:33:42 -0800

    Initial commit
    Signed-off-by: Zack Koppert <zkoppert@github.com>

---
+++ crawler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import github3
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':
+ 
+    # Load env variables from file
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    #Auth to GitHub.com
+    gh = github3.login(token=os.getenv('GH_TOKEN'))
+    
+    #Get all repos from organization
+    all_repos = gh.repositories_by(os.getenv('ORGANIZATION'))
+
+    #Print each repository
+    #TODO: Need to get private repos too
+    for repo in all_repos:
+        print(repo)
