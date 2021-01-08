66567e74bdbadfb701f2eb9297bde487a685ae37
Zack Koppert <zkoppert@github.com>
2021-01-08 15:37:04 -0800

    Add repo name printing
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- crawler.py
+++ crawler.py
@@ -12,12 +12,19 @@ if __name__ == '__main__':
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
-    #Auth to GitHub.com
+    # Auth to GitHub.com
     gh = github3.login(token=os.getenv('GH_TOKEN'))
     
-    #Get all repos from organization
-    all_repos = gh.repositories(os.getenv('ORGANIZATION'))
+    # Get all repos from organization
+    all_repos = gh.repositories()
 
-    #Print each repository
+    # TODO: Gather metadata for each repo
+
+    # TODO: Write each repository to a repos.json file
+    f = open("repos.json", "x")
     for repo in all_repos:
-        print(repo)
+        f.write(repo.name + '\n')
+        #for topic in repo.topics():
+            #if topic == 'inner-source':
+            #    f.write(repo.name + '\n')
+    f.close()
