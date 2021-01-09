f96c002c722fd68f3f92a0cde34c6f41465a5b48
Zack Koppert <zkoppert@github.com>
2021-01-08 18:32:06 -0800

    filter for innersource topics
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- crawler.py
+++ crawler.py
@@ -5,7 +5,6 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-
 if __name__ == '__main__':
  
     # Load env variables from file
@@ -22,9 +21,18 @@ if __name__ == '__main__':
 
     # TODO: Write each repository to a repos.json file
     f = open("repos.json", "x")
+
     for repo in all_repos:
-        f.write(repo.name + '\n')
-        #for topic in repo.topics():
-            #if topic == 'inner-source':
-            #    f.write(repo.name + '\n')
+        if repo is not None:
+            # Get the repo topics as type dict and print them nicely
+
+            #TODO: handle rate limiting
+            try:
+                repo_topic = repo.topics()
+            except:
+                print('skipping 404')
+            else:
+                if 'inner-source' in repo_topic.names:
+                    print('{0}'.format(repo))
+                    f.write(repo.name + '\n')
     f.close()
