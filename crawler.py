ba3c406d2331f9522d3b815ef847dfeb708fb8b6
Zack Koppert <zkoppert@github.com>
2021-01-21 22:02:44 +0000

    Build list of dicts and then convert and write json once
    Signed-off-by: GitHub <noreply@github.com>

--- crawler.py
+++ crawler.py
@@ -17,31 +17,33 @@ if __name__ == "__main__":
     gh = github3.login(token=os.getenv("GH_TOKEN"))
 
     # Get all repos from organization
-    all_repos = gh.repositories(type="private")
+    all_repos = gh.repositories()
 
+    repo_list = []
     # Set the topic
     topic = os.getenv("TOPIC")
-
-    # TODO: Write each repository to a repos.json file
-    f = open("repos.json", "w")
 
     for repo in all_repos:
         if repo is not None:
             # Get the repo topics as type dict and print them nicely
 
-            # TODO: handle rate limiting
+            # TODO: #6 handle rate limiting
             try:
                 repo_topic = repo.topics()
             except Exception:
                 print("skipping 404")
             else:
                 if topic in repo_topic.names:
-                    # TODO: For each resulting project add a key _InnerSourceMetadata to the result from the GitHub API call and fill it with additional metadata about the project:
+                    # TODO: #7 For each resulting project add a key _InnerSourceMetadata to the result from the GitHub API call and fill it with additional metadata about the project:
                     # TODO: - Check if there is a file innersource.json in the repository and add all keys directly below _InnerSourceMetadata.
                     # TODO: - Query GitHub for the weekly commit count (subset "all") and add it with the key participation
                     # TODO: - Check if there is a file CONTRIBUTING.md in the repository and add it with the key guidelines, value CONTRIBUTING.md
                     # TODO: Example available at https://github.com/SAP/project-portal-for-innersource/blob/main/repos.json
                     print("{0}".format(repo))
                     full_repository = repo.refresh()
-                    json.dump(full_repository._json_data, f, indent=2)
-    f.close()
+                    # Add stuff here about innersource.json data before appending to list
+                    repo_list.append(full_repository.as_dict())
+
+    # Write each repository to a repos.json file
+    with open("repos.json", "w") as f:
+        json.dump(repo_list, f, indent=4)
