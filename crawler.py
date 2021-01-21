da82b22a3256fd881f5f5f260fc6ed5c86ffdcd7
Zack Koppert <zkoppert@github.com>
2021-01-21 22:16:10 +0000

    remove extra comments and reorder imports
    Signed-off-by: GitHub <noreply@github.com>

--- crawler.py
+++ crawler.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
+import json
 import os
 from os.path import dirname, join
 
 import github3
 from dotenv import load_dotenv
-import json
 
 if __name__ == "__main__":
 
@@ -34,11 +34,7 @@ if __name__ == "__main__":
                 print("skipping 404")
             else:
                 if topic in repo_topic.names:
-                    # TODO: #7 For each resulting project add a key _InnerSourceMetadata to the result from the GitHub API call and fill it with additional metadata about the project:
-                    # TODO: - Check if there is a file innersource.json in the repository and add all keys directly below _InnerSourceMetadata.
-                    # TODO: - Query GitHub for the weekly commit count (subset "all") and add it with the key participation
-                    # TODO: - Check if there is a file CONTRIBUTING.md in the repository and add it with the key guidelines, value CONTRIBUTING.md
-                    # TODO: Example available at https://github.com/SAP/project-portal-for-innersource/blob/main/repos.json
+                    # TODO: #7 For each resulting project add a key _InnerSourceMetadata
                     print("{0}".format(repo))
                     full_repository = repo.refresh()
                     # Add stuff here about innersource.json data before appending to list
