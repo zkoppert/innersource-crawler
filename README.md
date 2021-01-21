3730f4b41ffb2526b6c6a11341f2c79e7461c2a7
Zack Koppert <zkoppert@github.com>
2021-01-21 01:04:28 +0000

    Change file from text to json data
    Signed-off-by: GitHub <noreply@github.com>

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # InnerSource Crawler
 ![.github/workflows/linter.yml](https://github.com/zkoppert/innersource-crawler/actions/workflows/linter.yml/badge.svg) ![Integration Test](https://github.com/zkoppert/innersource-crawler/actions/workflows/integration_tests.yml/badge.svg)
 
-This project creates a repos.json that can be utilized by the SAP InnerSource Portal.
+This project creates a repos.json that can be utilized by the [SAP InnerSource Portal](https://github.com/sap/project-portal-for-InnerSource).
 
 ## Installation
 `pip -r requirements.txt`
 
 ## Usage
 1. Fill out the .env file with a token from a machine user that only has access to the org to scan
-1. Fill out the .env file with the exact topic name you are searching for 
+1. Fill out the .env file with the exact topic name you are searching for
 1. Run `python3 ./crawler.py`
--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ from os.path import dirname, join
 
 import github3
 from dotenv import load_dotenv
+import json
 
 if __name__ == "__main__":
 
@@ -16,9 +17,7 @@ if __name__ == "__main__":
     gh = github3.login(token=os.getenv("GH_TOKEN"))
 
     # Get all repos from organization
-    all_repos = gh.repositories()
-
-    # TODO: Gather metadata for each repo
+    all_repos = gh.repositories(type="private")
 
     # Set the topic
     topic = os.getenv("TOPIC")
@@ -37,6 +36,12 @@ if __name__ == "__main__":
                 print("skipping 404")
             else:
                 if topic in repo_topic.names:
+                    # TODO: For each resulting project add a key _InnerSourceMetadata to the result from the GitHub API call and fill it with additional metadata about the project:
+                    # TODO: - Check if there is a file innersource.json in the repository and add all keys directly below _InnerSourceMetadata.
+                    # TODO: - Query GitHub for the weekly commit count (subset "all") and add it with the key participation
+                    # TODO: - Check if there is a file CONTRIBUTING.md in the repository and add it with the key guidelines, value CONTRIBUTING.md
+                    # TODO: Example available at https://github.com/SAP/project-portal-for-innersource/blob/main/repos.json
                     print("{0}".format(repo))
-                    f.write(repo.name + "\n")
+                    full_repository = repo.refresh()
+                    json.dump(full_repository._json_data, f, indent=2)
     f.close()
