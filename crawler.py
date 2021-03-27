f51c4524d941839b3ba82f4be4fe1bb2d1a23e06
Jason Macgowan <1389531+jasonmacgowan@users.noreply.github.com>
2021-03-27 06:47:20 -0400

    run black

--- crawler.py
+++ crawler.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from base64 import b64decode
 import json
 import os
 from os.path import dirname, join
+from base64 import b64decode
 
 import github3
 from dotenv import load_dotenv
@@ -32,34 +32,38 @@ if __name__ == "__main__":
             full_repository = repo.repository.refresh()
 
             innersource_repo = repo.as_dict()
-            innersource_repo['_InnerSourceMetadata'] = {}
-            
+            innersource_repo["_InnerSourceMetadata"] = {}
+
             # fetch innersource.json
             try:
-                content = repo.repository.file_contents('/innersource.json').content
+                content = repo.repository.file_contents("/innersource.json").content
                 metadata = json.loads(b64decode(content))
 
-                innersource_repo['_InnerSourceMetadata'] = metadata
+                innersource_repo["_InnerSourceMetadata"] = metadata
             except github3.exceptions.NotFoundError:
                 # innersource.json not found in repository, but it's not required
                 pass
 
             # fetch repository participation
             participation = repo.repository.weekly_commit_count()
-            innersource_repo['_InnerSourceMetadata']['participation'] = participation['all']
+            innersource_repo["_InnerSourceMetadata"]["participation"] = participation[
+                "all"
+            ]
 
             # fetch contributing guidelines
             try:
                 # if CONTRIBUTING.md exists in the repository, link to that instead of repo root
-                content = repo.repository.file_contents('/CONTRIBUTING.md').content
-                innersource_repo['_InnerSourceMetadata']['guidelines'] = 'CONTRIBUTING.md'
+                content = repo.repository.file_contents("/CONTRIBUTING.md").content
+                innersource_repo["_InnerSourceMetadata"][
+                    "guidelines"
+                ] = "CONTRIBUTING.md"
             except github3.exceptions.NotFoundError:
                 # CONTRIBUTING.md not found in repository, but it's not required
                 pass
 
             # fetch repository topics
             topics = repo.repository.topics()
-            innersource_repo['_InnerSourceMetadata']['topics'] = topics.names
+            innersource_repo["_InnerSourceMetadata"]["topics"] = topics.names
 
             repo_list.append(innersource_repo)
 
